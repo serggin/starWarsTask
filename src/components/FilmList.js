2a87751895b186ac7007619d2b986b4e9a6312bb
Serggin <serggin1@yandex.ru>
2020-09-10 19:46:54 +0300

    FilmList draft

--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Paper, Typography } from '@material-ui/core';
+import { Paper, Typography, List, ListItem, ListItemText, Divider } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
 
@@ -12,16 +12,50 @@ const useStyles = makeStyles(theme => ({
   paper: {
 
   },
+  list: {
+
+  },
+  titleButton: {
+    cursor: 'pointer',
+  },
+  itemText: {
+
+  },
+  itemTextNotSelected: {
+    textDecoration: 'underline',
+  },
 }));
 
-const FilmList = (props) => {
-  const classes = useStyles();
-  console.log('FilmList props=', props);
+const ListItemButton = props => <ListItem button {...props} />;
 
+
+const FilmList = ({titles}) => {
+  const [keySelected, SetKeySelected] = useState(undefined);
+  const classes = useStyles();
+  //console.log('FilmList titles=', titles);
+  const keys = Object.keys(titles);
+  const onSelect = (key) => { SetKeySelected(key)}
   return (
     <div className={classes.root}>
       <Paper className={classes.paper}>
-        <Typography>FilmList | }</Typography>
+        {keys.length > 0 &&
+          <>
+            <div className={classes.list}>
+              <List>
+                {keys.map(key => (
+                  <ListItemButton className={classes.titleButton} key={key} onClick={() => onSelect(key)}
+                  disabled={keySelected && keySelected === key}>
+                    <ListItemText
+                      className={`${classes.itemText} ${(!keySelected || keySelected !== key) && classes.itemTextNotSelected}`}
+                      primary={titles[key]} />
+                  </ListItemButton>
+                ))}
+              </List>
+            </div>
+            <Divider />
+            <Typography>Select episode</Typography>
+          </>
+        }
       </Paper>
     </div>
   );
